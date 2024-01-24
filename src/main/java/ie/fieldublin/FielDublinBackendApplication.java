239a15269f09047f0c27bd6365cdec3c2b1114cc
Rafael Infante <rafael@telclic.net>
2024-01-24 15:13:15 +0000

    Initial commit

---
+++ src/main/java/ie/fieldublin/FielDublinBackendApplication.java
@@ -0,0 +1,13 @@
+package ie.fieldublin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FielDublinBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FielDublinBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/fieldublin/FielDublinBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.fieldublin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FielDublinBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

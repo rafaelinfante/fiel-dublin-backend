19be6f2cdc48e17c133e0fc9d36c9bc4be49a704
Rafael Infante <rafael@telclic.net>
2024-01-24 16:42:18 +0000

    Adding Flyway

---
+++ src/main/java/ie/fieldublin/config/AppConfig.java
@@ -0,0 +1,18 @@
+package ie.fieldublin.config;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public FlywayMigrationStrategy cleanMigrateStrategy() {
+        return flyway -> {
+            flyway.repair();
+            flyway.migrate();
+        };
+    }
+
+}
